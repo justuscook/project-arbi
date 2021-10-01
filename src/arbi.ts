53d6725c522be6b9aea7bb247be9446d376b82d2
justuscook <justus_cook@yahoo.com>
2021-10-01 10:19:49 -0500

    update to add commands to multiple test servers by ID

--- src/arbi.ts
+++ src/arbi.ts
@@ -1,6 +1,6 @@
 import { ApplicationCommand, Client, ClientApplication, Collection, CommandInteraction, Guild, OAuth2Guild, Snowflake, Intents, ApplicationCommandResolvable } from 'discord.js';
 import fs from 'fs';
-const { clientId, RaidServer, TestServer, token } = require('./config.json');
+const { clientId, guildIDs, token } = require('./config.json');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 
@@ -16,7 +16,7 @@ for (const file of commandFiles) {
 
 client.once('ready', async () => {
     console.log('Ready!');
-    deployCommands();
+    await deployCommands();
 });
 
 client.on('interactionCreate', async interaction => {
@@ -38,7 +38,6 @@ client.login(token);
 
 
 async function deployCommands() {
-    const server = TestServer;//TestServer or Raid Server
     const commands = [];
     const commandFiles = fs.readdirSync(__dirname + '/commands').filter(file => file.endsWith('.js'));
 
@@ -63,38 +62,26 @@ async function deployCommands() {
     */
     (async () => {
         try {
-            const response = await rest.put(
-                Routes.applicationGuildCommands(clientId, RaidServer),
-                { body: commands },
-            );
-
-            if (!client.application?.owner) await client.application?.fetch();
-            for (const r of response) {
-                const command = await client.guilds.cache.get(RaidServer)?.commands.fetch(r.id);
-                const commandFile = require(__dirname + `/commands/${r.name}`);
-                if (command.defaultPermission === false) {
-                    const permissions = commandFile.permissions
-                    await command.permissions.set({ permissions });
+            for (const g of guildIDs) {
+                const response = await rest.put(
+                    Routes.applicationGuildCommands(clientId, g),
+                    { body: commands },
+                );
+                const guild: Guild = await client.guilds.cache.get(g);
+                if (!client.application?.owner) await client.application?.fetch();
+                for (const r of response) {
+                    const command = await guild?.commands.fetch(r.id);
+                    const commandFile = require(__dirname + `/commands/${r.name}`);
+                    if (command.defaultPermission === false) {
+                        const permissions = commandFile.permissions
+                        await command.permissions.set({ permissions });
+                    }
                 }
+                //console.log(response2)
+                console.log(`Successfully registered application commands in ${guild.name}.`);
             }
-
-            const response2 = await rest.put(
-                Routes.applicationGuildCommands(clientId, TestServer),
-                { body: commands },
-            );
-
-            if (!client.application?.owner) await client.application?.fetch();
-            for (const r of response2) {
-                const command = await client.guilds.cache.get(TestServer)?.commands.fetch(r.id);
-                const commandFile = require(__dirname + `/commands/${r.name}`);
-                if (command.defaultPermission === false) {
-                    const permissions = commandFile.permissions
-                    await command.permissions.set({ permissions });
-                }
-            }
-            //console.log(response2)
-            console.log('Successfully registered application commands.');
-        } catch (error) {
+        }
+        catch (error) {
             console.error(error);
         }
     })();
